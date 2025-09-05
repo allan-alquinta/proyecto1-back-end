8e94a7a4406f4612916a317c551a049937c61b20
allan-alquinta <juniorsaid2002@gmail.com>
2025-09-05 16:23:41 -0400

    commit 3 registro de modelos en admin + creación de superusuario

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cliente, Tarifa, Contrato, Medidor, Lectura, Boleta, Pago, Notificacion
 
-# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Tarifa)
+admin.site.register(Contrato)
+admin.site.register(Medidor)
+admin.site.register(Lectura)
+admin.site.register(Boleta)
+admin.site.register(Pago)
+admin.site.register(Notificacion)
